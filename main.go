805f32122962bcbd9f57503b9b4878548b3ac9ff
slwnn <viacheslav.aleshin@pepperstone.com>
2025-04-11 12:18:33 +0300

    Init app

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-test file
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from Go!")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	fmt.Println("Starting server on :3000")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatalf("Server failed: %s", err)
+	}
+}
+
